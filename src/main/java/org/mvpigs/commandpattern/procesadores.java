1d2edcef780035ae1c72668aa5b7a10577a358ad
draeriel <danieljuanbarbosa@gmail.com>
2018-06-07 17:59:04 +0200

    Oficina de correso abierta con éxito

--- src/main/java/org/mvpigs/commandpattern/procesadores.java
+++
@@ -1,4 +0,0 @@
-package org.mvpigs.commandpattern;
-
-public class procesadores {
-}
---
+++ src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
@@ -0,0 +1,16 @@
+package org.mvpigs.commandpattern.procesadores;
+
+import org.mvpigs.commandpattern.interfaces.Procesador;
+import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
+
+public class Oficina implements Procesador {
+
+    public Oficina() {
+
+    }
+    public boolean procesa(TratamientoPedido pedido) {
+        return pedido.tratar();
+    }
+
+
+}
