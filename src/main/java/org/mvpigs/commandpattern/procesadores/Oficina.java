f1e6e72cab3fe77d03fbd1befd372f6aefeb8401
draeriel <danieljuanbarbosa@gmail.com>
2018-06-07 18:47:20 +0200

    Por fin la Oficina evalúa

--- src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
+++ src/main/java/org/mvpigs/commandpattern/procesadores/Oficina.java
@@ -1,5 +1,6 @@
 package org.mvpigs.commandpattern.procesadores;
 
+import org.mvpigs.commandpattern.interfaces.Pedido;
 import org.mvpigs.commandpattern.interfaces.Procesador;
 import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
 
@@ -11,6 +12,29 @@ public class Oficina implements Procesador {
     public boolean procesa(TratamientoPedido pedido) {
         return pedido.tratar();
     }
+    public String printarStatus( boolean status, Pedido pedido) {
+
+        int estado = (status)?1:0;
+        int result = 0;
+        return (pedido.destino() + " " + ((estado == 0)?"RECHAZADO":"ACEPTADO"));
+    }
 
 
-}
+
+
+    private enum Status {
+        RECHAZADO (0),
+        ACEPTADO (1);
+
+        private final int numero;
+
+        Status(int numero){
+            this.numero = numero;
+        }
+
+        public int getNumero(){
+            return this.numero;
+        }
+
+
+}}
